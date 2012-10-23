a97a92491f95c3a0f8002be64b533c4e79f27811
Morgan Herlocker <morgan.herlocker@gmail.com>
2012-10-23 12:10:40 -0400

    home, login, and register page added

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,21 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'xory' });
+};
+
+/*
+ * GET login page.
+ */
+
+exports.login = function(req, res){
+  res.render('login');
+};
+
+/*
+ * GET register page.
+ */
+
+exports.register = function(req, res){
+  res.render('register');
 };
